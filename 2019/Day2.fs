8982bc5a8b3d4753079effdfd92f555ca7bc9a98
Stan Janssen <mail@janssen.io>
2019-12-02 12:18:19 +0100

    Prepare for possible future puzzles

--- 2019/Day2.fs
+++ 2019/Day2.fs
@@ -1,63 +1,88 @@
 ﻿namespace AdventOfCode2019
 
-open System
-
 module Day2 = 
 
     open Xunit
     open FsCheck
     open FsCheck.Xunit
 
-    let maybeDo f a b =
-        match f with
-        | Some op -> Some <| op a b
-        | None    -> None
+    type Memory = int array
 
-    let getOp i =
-        match i with
-        |  1 -> Some (+)
-        |  2 -> Some ((*) : int -> int -> int)
-        | 99 -> None
-        |  x -> failwithf "Unexpected operation: %i" x
+    type Operation =
+        | Sum
+        | Product
+        | NoOp
 
-    let run (s:int array) noun verb =
-        s.[1] <- noun
-        s.[2] <- verb
-        let rec run' (stack:int array) pc =
-            let op = getOp stack.[pc]
-            match op with
-            | None -> stack
-            | Some f ->
-                let  a = stack.[pc + 1]
-                let  b = stack.[pc + 2]
-                let  p = stack.[pc + 3]
-                stack.[p] <- f stack.[a] stack.[b]
-                run' stack (pc + 4)
-        run' s 0
+    type Instruction = Operation * int list
+
+    type State = {
+        Memory : Memory
+        InstructionPointer : int
+    }
+
+    let parseInstruction instruction =
+        match instruction with
+        | 1     -> Sum
+        | 2     -> Product
+        | 99    -> NoOp
+        | _     -> failwithf "Unknown instruction %i" instruction
+
+    let arity instruction =
+        match instruction with
+        | Sum | Product -> 2
+        | NoOp  -> 0
+
+    let getParameters instruction (memory : Memory) ip =
+        seq { for p in [1..arity instruction] do yield memory.[ip + p] }
+        |> Seq.map (fun i -> memory.[i])
+        |> List.ofSeq
+
+    let perform (instruction, parameters) =
+        parameters
+        |> match instruction with
+            | Sum     -> List.fold (+) 0 
+            | Product -> List.fold (*) 1
+            | NoOp    -> failwithf "Cannot perform %A" instruction
+
+    let run'' (input:int array) noun verb =
+        let memory' = Array.copy input
+        memory'.[1] <- noun
+        memory'.[2] <- verb
+        let rec computer state =
+            let instruction = parseInstruction state.Memory.[state.InstructionPointer]
+            match instruction with
+            | NoOp -> state
+            | _    ->
+                let ip = state.InstructionPointer 
+                let parameters = getParameters instruction state.Memory ip
+                let position = state.Memory.[ip + arity instruction + 1]
+                state.Memory.[position] <- perform (instruction, parameters)
+                { state with
+                    Memory = state.Memory 
+                    InstructionPointer = state.InstructionPointer + arity instruction + 2 }
+                |> computer
+        computer { Memory = memory'; InstructionPointer = 0 }
+        |> (fun s -> s.Memory)
 
     let input = [|1;12;2;3;1;1;2;3;1;3;4;3;1;5;0;3;2;1;10;19;1;19;6;23;2;23;13;27;1;27;5;31;2;31;10;35;1;9;35;39;1;39;9;43;2;9;43;47;1;5;47;51;2;13;51;55;1;55;9;59;2;6;59;63;1;63;5;67;1;10;67;71;1;71;10;75;2;75;13;79;2;79;13;83;1;5;83;87;1;87;6;91;2;91;13;95;1;5;95;99;1;99;2;103;1;103;6;0;99;2;14;0;0|]
 
     let runPartTwo (s:int array) =
-        let mutable x = (0, 0)
         let mutable r = (0, 0)
         for noun in 0..99 do
             for verb in 0..99 do
-                let memory = Array.copy s
-                if (run memory noun verb).[0] = 19690720 then
+                if (run'' s noun verb).[0] = 19690720 then
                     r <- (noun, verb)
-                else
-                    x <- (noun, verb)
-        r 
+                else ()
+        fst r * 100 + snd r
                     
-                    
-
     [<Property>]
     let ``Part one: Add`` (a:PositiveInt) (b:PositiveInt) (p:PositiveInt) =
         let x = a.Get % 5
         let y = b.Get % 5
-        let input = [|1; x; y; p.Get % 3; 99|]
+        let input = [|1; x; y; p.Get % 4; 99|]
+        let output = run'' input x y
         let expected = input.[x] + input.[y]
-        let actual = (run input x y).[p.Get % 3]
+        let actual = output.[p.Get % 4]
         expected = actual
 
     [<Property>]
@@ -66,14 +91,18 @@ module Day2 =
         let y = b.Get % 5
         let input = [|2; x; y; p.Get % 3; 99|]
         let expected = input.[x] * input.[y]
-        let actual = (run input x y).[p.Get % 3]
+        let actual = (run'' input x y).[p.Get % 3]
         expected = actual
 
     [<Fact>]
-    let ``Part one: example`` () =
-        let output = run [|1;9;10;3;2;3;11;0;99;30;40;50|] 9 10
+    let ``Part one: multiple operations`` () =
+        let output = run'' [|1;9;10;3;2;3;11;0;99;30;40;50|] 9 10
         Assert.True([|3500;9;10;70;2;3;11;0;99;30;40;50|] = output)
 
     [<Fact>]
     let ``Part one: answer`` () =
-        Assert.Equal(3790645, (run input 12 2).[0])
+        Assert.Equal(3790645, (run'' input 12 2).[0])
+
+    [<Fact>]
+    let ``Part two: answer`` () =
+        Assert.Equal(6577, (runPartTwo input))
