59147cba17722399512aaf265decfdbbb066b360
Stan Janssen <mail@janssen.io>
2021-12-07 20:34:42 +0100

    2021 Day 01 in Prolog

---
+++ 2021/01.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/swipl -f -q
+
+:- initialization(main, main).
+
+main() :-
+    increments([], 0, Count), # Add input here
+    write(Count).
+
+increments([_|[_|[_]]], C, C).
+increments([X|[Y|[Z|[A|As]]]], Acc, Count) :-
+    X < A,
+    Acc1 is Acc + 1,
+    increments([Y|[Z|[A|As]]], Acc1, Count).
+
+increments([X|[Y|[Z|[A|As]]]], Acc, Count) :-
+    X >= A,
+    increments([Y|[Z|[A|As]]], Acc, Count).
+
+
