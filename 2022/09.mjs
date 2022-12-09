7423e9a39ee4d26b608e7f8ac6b99c87952df4e9
Stan Janssen <mail@janssen.io>
2022-12-09 18:29:39 +0100

    2022 Day 09 refactored

--- 2022/09.mjs
+++ 2022/09.mjs
@@ -1,56 +1,43 @@
 import { readAndSolve, range } from '../aoc.mjs'
 
-function move({ x, y }, [dx, dy]) {
-    return { x: x + dx, y: y + dy };
-}
-
-function solve(lines) {
-    let gridT = { dimensions: { max_x: 6, max_y: 6, min_x: -6, min_y: -6}};
-    let Head = { x: 0, y: 0 };
-    let p1 = [{x: 0, y: 0}]
-    let p2 = range(0, 9).map(_ => { return { x: 0, y: 0 } });
+function solveFor(lines, ropeLength) {
+    const gridT = {};
+    const rope = range(0, ropeLength).map(_ => { return { x: 0, y: 0 } });
 
     gridT.setCell(0, 0, '#');
-    let snake = [Head, ...p2];
 
     for (let line of lines) {
-        let [dir, num] = line.split(' ');
+        const [dir, num] = line.split(' ');
 
+        // break down the move (e.g., R 5) into one-step pieces
         for (let remainingMoves = num; remainingMoves > 0; remainingMoves--) {
+            // move the head first
             switch (dir) {
-                case 'U':
-                    snake[0] = move(snake[0], [0, 1])
-                    break;
-                case 'D':
-                    snake[0] = move(snake[0], [0, -1])
-                    break;
-                case 'R':
-                    snake[0] = move(snake[0], [1, 0])
-                    break;
-                case 'L':
-                    snake[0] = move(snake[0], [-1, 0])
-                    break;
+                case 'U': rope[0].y++; break;
+                case 'D': rope[0].y--; break;
+                case 'R': rope[0].x++; break;
+                case 'L': rope[0].x--; break;
             }
-            for(let headIndex = 0; headIndex < snake.length - 1; headIndex++) {
-                const H = snake[headIndex]
-                let T = snake[headIndex+1]
+
+            // move the tail piece by piece
+            for (let headIndex = 0; headIndex < rope.length - 1; headIndex++) {
+                const H = rope[headIndex];
+                const T = rope[headIndex + 1];
                 if (Math.abs(H.y - T.y) > 1 || Math.abs(H.x - T.x) > 1) {
-                    // T is left
-                    if (H.x > T.x) { T.x += 1 }
-                    // T is right
-                    if (H.x < T.x) { T.x -= 1 }
-                    // T is up
-                    if (H.y < T.y) { T.y -= 1 }
-                    // T is down
-                    if (H.y > T.y) { T.y += 1 }
-                    snake[headIndex + 1] = T;
+                    if (H.x > T.x) { T.x++; } // T is left
+                    if (H.x < T.x) { T.x--; } // T is right
+                    if (H.y < T.y) { T.y--; } // T is up
+                    if (H.y > T.y) { T.y++; } // T is down
                 }
             }
-            gridT.setCell(snake.last().x, -snake.last().y, '#')
+            gridT.setCell(rope.last().x, -rope.last().y, '#')
         }
     }
     return Object.values(gridT).filter(x => x === '#').length;
 }
 
+const solve = (lines) => {
+    return { p1: solveFor(lines, 2), p2: solveFor(lines, 10) }
+}
 
 readAndSolve(process.argv[2] || '09.input', solve, '\n');
