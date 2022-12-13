7f2182b06ff9f4b8bc0ae4ff233d70d388e9c6db
Stan Janssen <mail@janssen.io>
2022-12-13 06:56:05 +0100

    2022 Day 13 p2

--- 2022/13.mjs
+++ 2022/13.mjs
@@ -4,11 +4,11 @@ function compare(leftItem, rightItem, debug) {
     if (debug) console.log({leftItem, rightItem})
     if (leftItem === undefined) {
         if (rightItem === undefined) return 0;
-        console.log("left ran out")
+        // console.log("left ran out")
         return 1;
     }
     else if (rightItem === undefined) {
-        console.log("right ran out")
+        // console.log("right ran out")
         return -1;
     }
 
@@ -38,24 +38,22 @@ function compare(leftItem, rightItem, debug) {
     }
 }
 
-const solveFor = (lines) => {
-    const indices = [];
-    for(let index = 0; index < lines.length; index++) {
-        const pair = lines[index];
-        const left_right = pair.split('\n');
-        const [left, right] = left_right.map(eval);
-
-        let result = 0;
-        for(let i = 0; i <= left.length && result === 0; i++) {
-            result = compare(left[i], right[i], false);
-        }
-        // console.log({index, result}, '\n')
-        if(result != -1) {
-            indices.push(index + 1) // +1 because data is 1-indexed;
-        }
+function packetSort(left, right) {
+    let result = 0;
+    for(let i = 0; i <= left.length && result === 0; i++) {
+        result = compare(left[i], right[i], false);
     }
-    console.log(indices)
-    return indices.sum();
+    return result;
+}
+
+const solveFor = (lines) => {
+    lines = lines.filter(x => x !== '');
+    lines.push('[[2]]')
+    lines.push('[[6]]');
+    const packets = lines.map(eval).sort(packetSort).reverse();
+    const a = packets.indexOfP(x => JSON.stringify(x) == '[[2]]') + 1
+    const b = packets.indexOfP(x => JSON.stringify(x) == '[[6]]') + 1
+    return a * b;
 }
 
 const solve = (lines) => {
@@ -66,13 +64,13 @@ const solve = (lines) => {
 }
 
 (async () => {
-    let example = await readAndSolve('13.example.input', solve, '\n\n');
+    let example = await readAndSolve('13.example.input', solve, '\n');
     test('Example p1', 13, example.answer.p1)
     // test('Example p2', 29, example.answer.p2)
 
     console.log();
 
-    const puzzle = await readAndSolve(process.argv[2] || '13.input', solve, '\n\n');
+    const puzzle = await readAndSolve(process.argv[2] || '13.input', solve, '\n');
     test('Incorrect p1 (should be red)', 5742, puzzle.answer.p1)
     test('Incorrect p1 (should be red)', 2853, puzzle.answer.p1)
     test('Incorrect p1 (should be red)', 3773, puzzle.answer.p1)
