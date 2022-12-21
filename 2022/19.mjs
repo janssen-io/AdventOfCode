f01d1a97fd9c467fc2aacd8c78d3b3d05aab7fe6
Stan Janssen <mail@janssen.io>
2022-12-21 07:38:49 +0100

    2022 Day 19 p2

--- 2022/19.mjs
+++ 2022/19.mjs
@@ -1,5 +1,4 @@
 import { readAndSolve, printAnswer, test, search, range, assert, bfs, dfs } from '../aoc.mjs'
-import { Heap } from '../heap.mjs'
 
 function parse(line) {
     const data = line.numbers();
@@ -38,44 +37,49 @@ function build(type, robot, state) {
     return state;
 }
 
-let rejected = 0;
-let skipped = 0;
 let times = new Set();
-let maxGeode = {};
-
-let geodeOne = {};
-
-function nextStates(blueprint, timeLimit, heap) {
+let maxGeode = 0;
+let maxGeodeT = {};
+function nextStates(blueprint) {
+    console.log({ bp: blueprint.robots, cost: blueprint.maxCost })
     return function (state) {
         const next = [];
+        maxGeodeT[state.time] = maxGeodeT[state.time] || 0;
+        maxGeodeT[state.time] = Math.max(maxGeodeT[state.time], state.rocks.geode);
         maxGeode = Math.max(maxGeode, state.rocks.geode);
-        // if (state.rocks.geode < maxGeode && !canBuild(blueprint.robots.geode, state.rocks)) {
-        //     return next;
-        // }
-        // If we can build a geode-cracker, then only do that this step.
-        // Greed is good?
-        // if (canBuild(blueprint.robots.geode, state.rocks)) {
-        //     next.push(build('geode', blueprint.robots.geode, state.clone()));
-        // }
-        // else {
-        for (const type of ['geode', 'obsidian', 'clay', 'ore']) {
+
+        if (state.time > 0 && state.rocks.geode < maxGeodeT[state.time]) return next;
+
+        // save up for something
+        next.push(state.clone());
+
+        if (canBuild(blueprint.robots.geode, state.rocks)) {
+            const stateWithNewRobot = build('geode', blueprint.robots.geode, state.clone());
+            next.push(stateWithNewRobot);
+        }
+        else if(canBuild(blueprint.robots.obsidian, state.rocks)){
+            const stateWithNewRobot = build('obsidian', blueprint.robots.obsidian, state.clone());
+            next.push(stateWithNewRobot);
+        }
+        else
+        // or build something if we can
+        for (const type of ['clay', 'ore']) {
             // for (const type of ['geode', 'obsidian', 'clay', 'ore']) {
             // don't make more robots than we can spend in one turn
-            if (type !== 'geode' && blueprint.maxCost[type] <= state.robots[type]) continue;
+            // if (type !== 'geode' && blueprint.maxCost[type] <= state.robots[type]) continue;
             if (!canBuild(blueprint.robots[type], state.rocks)) continue;
 
-            // if we could have built this last turn, we should have. Instead we are saving for something bigger.
+            // if we could have built this last turn, we should have. 
+            // instead we are saving for something bigger.
             if (canBuild(blueprint.robots[type], {
                 ore: state.rocks.ore - state.robots.ore,
                 clay: state.rocks.clay - state.robots.clay,
                 obsidian: state.rocks.obsidian - state.robots.obsidian,
             })) continue;
 
-            next.push(build(type, blueprint.robots[type], state.clone()));
+            const stateWithNewRobot = build(type, blueprint.robots[type], state.clone());
+            next.push(stateWithNewRobot);
         }
-        // doing nothing is also a valid next state
-        next.push(state.clone());
-        // }
 
         for (const n of next) {
             n.time++;
@@ -87,22 +91,22 @@ function nextStates(blueprint, timeLimit, heap) {
             }
         }
 
-        maxGeode = Math.max(maxGeode, ...next.map(s => s.rocks.geode));
-        return next;
+        // maxGeode = Math.max(maxGeode, ...next.map(s => s.rocks.geode));
+        // return next;
+        return next.filter(x => x.rocks.geode >= maxGeodeT[state.time]);
     }
 }
 
 function simulate(blueprint, timeLimit) {
-    console.log('sim:', blueprint.id);
     maxGeode = 0;
+    maxGeodeT = {};
     times.clear();
-    skipped = 0; rejected = 0;
-    const sw = new Date();
 
+    console.log('sim:', blueprint.id);
     const isMatch = s => s.time == timeLimit;
     // How much of everything can we still make if we would just idle
     // const getKey = s => JSON.stringify(remainingCapacity(blueprint, timeLimit, s));
-    const getKey = s => JSON.stringify(s);
+    const getKey = s => JSON.stringify([s.time,s.rocks,s.robots]);
     let show = s => s;
     show = false
 
@@ -118,12 +122,11 @@ function simulate(blueprint, timeLimit) {
         time: 0
     }
 
-
     init.robots.ore = 1;
 
-    Array.from(bfs([init], nextStates(blueprint, timeLimit), isMatch, getKey, show));
-    const top = maxGeode;
-    console.log({ id: blueprint.id, top, total: total = total + blueprint.id * top, t: new Date() - sw });
+    const solutions = Array.from(bfs([init], nextStates(blueprint, timeLimit), isMatch, getKey, show));
+    const top = solutions.numSortBy(true, s => s.state.rocks.geode)[0]?.state.rocks.geode || 0;
+    // console.log({ top, maxGeode, maxGeodeT });
     return top;
 }
 
@@ -148,19 +151,18 @@ const solveFor = (lines, timeLimit) => {
 
 const solve = (lines) => {
     return {
-        p1: solveFor(lines, 24),
+        // p1: solveFor(lines, 24),
         p2: solveFor(lines, 32),
     }
 }
 
 (async () => {
-    let example = await readAndSolve('19.example.input', solve, '\n');
-    test('Example p1', 33, example.answer.p1)
-    test('Example p2', undefined, example.answer.p2)
-    assert(() => 33 == example.answer.p1, "p1")
+    // let example = await readAndSolve('19.example.input', solve, '\n');
+    // test('Example p1', 1 * 9 + 2 * 12, example.answer.p1)
+    // test('Example p2', 56 * 62, example.answer.p2)
+    // assert(() => 33 == example.answer.p1, "p1")
 
-    // 1760 -- too low
-    // 2259, 2264 -- wrong
-    // const puzzle = await readAndSolve('19.input', solve, '\n');
-    // printAnswer(puzzle);
+    // 1760, 3570 (17*7*30)-- too low
+    const puzzle = await readAndSolve('19.input', solve, '\n');
+    printAnswer(puzzle);
 })();
