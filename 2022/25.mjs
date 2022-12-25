a13a9040681c4b7eed996da0c16f414776b0809f
Stan Janssen <mail@janssen.io>
2022-12-25 07:38:23 +0100

    Little bit of a cleanup

--- 2022/25.mjs
+++ 2022/25.mjs
@@ -15,13 +15,6 @@ function solveFor(lines) {
         '-': -1,
         '=': -2,
     }
-    const snafus = {
-        '2': '2',
-        '1': '1',
-        '0': '0',
-        '-1': '-',
-        '-2': '=',
-    }
 
     const inverseSnafu = {
         '2': '=',
@@ -37,74 +30,54 @@ function solveFor(lines) {
         for (let i = 0; i < parts.length; i++) {
             const digit = digits[parts[i]];
             value = value + digit * Math.pow(5, i);
-            // console.log({digit, value})
         }
         return value;
     }
 
     const negate = (snafu) => snafu.split('').map(s => inverseSnafu[s]).join('');
 
-    /*
-    13
-    -25 (1) = -12
-
-    12
-    -10 (2) = 2
-
-    2 (2)
-
-    2==
-
-    
-
-    */
-    function decToSnafu(dec, depth = 0) {
+    function decToSnafu(dec) {
         let remainder = dec;
-        let sum = 0;
         let answer = ''
-        if (depth > 10) throw "stopped";
-        let e = 30;
-        for (; e >= 0; e--) {
+        for (let e = 30; e >= 0; e--) {
             const maxWithoutThisDigit = Math.floor(Math.pow(5, e) / 2)
 
+            // fill in the remaining 0s
+            // e + 1, because there is also a digit for e = 0
             if (remainder == 0) {
-                answer += '0'; // just finish the loop adding zeroes. 
+                answer = answer + '0'.repeat(e + 1);
+                break;
             }
-            else if (remainder < 0) {
-                const lastQuints = negate(decToSnafu(remainder * - 1, depth + 1));
+
+            if (remainder < 0) {
+                const lastQuints = negate(decToSnafu(remainder * - 1));
 
                 // Make sure to actually fill in the trimmed 0s between the current answer and the last quints.
-                return answer.replace(/^0+/, '') + '0'.repeat(e - lastQuints.length + 1) + lastQuints;
+                answer = answer + '0'.repeat(e - lastQuints.length + 1) + lastQuints;
+                break;
             }
-            else if (remainder <= maxWithoutThisDigit) {
-                answer += '0'
-            }
-            else if (Math.abs(remainder - Math.pow(5, e)) <= maxWithoutThisDigit) {
-                answer += '1'
-                const value = 1 * Math.pow(5, e);
-                sum += value;
-                remainder -= value;
-            }
-            else {
-                answer += '2'
-                const value = 2 * Math.pow(5, e);
-                sum += value;
-                remainder -= value;
+
+            // Check which smallest value can reduce the remainder, so that
+            // the remaining digits can still sum up to the result.
+            for(let i = 0; i <= 2; i++) {
+                if (Math.abs(remainder - i * Math.pow(5, e)) <= maxWithoutThisDigit) {
+                    answer += i
+                    const value = i * Math.pow(5, e);
+                    remainder -= value;
+                    break;
+                }
             }
         }
 
-        // Trim quints that literally don't add value.
+        // Trim leading zeroes otherwise they mess up the recursive result.
         const snafuString = answer.replace(/^0+/, '');
         return snafuString;
     }
 
     const p1_10 = lines.map(snafuToDec).sum();
-    // for (let i = 0; i < 10_000; i++) {
-    //     const p1_10 = i;
-        const p1 = decToSnafu(p1_10);
-        console.log(p1, snafuToDec(p1), p1_10);
-        assert(() => snafuToDec(p1) == p1_10, 'Invalid conversion')
-    // }
+    const p1 = decToSnafu(p1_10);
+    console.log(p1, snafuToDec(p1), p1_10);
+    assert(() => snafuToDec(p1) == p1_10, 'Invalid conversion')
     return p1;
 }
 
