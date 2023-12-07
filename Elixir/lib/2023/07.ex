e26fed0195d837108cb6a5ef2f36cff2bd461ec3
Stan Ionitoiu-Janssen <mail@janssen.io>
2023-12-07 18:50:22 +0100

    2023.7 Simplify sorted_type with frequencies

--- Elixir/lib/2023/07.ex
+++ Elixir/lib/2023/07.ex
@@ -65,28 +65,24 @@ defmodule Year2023.Day07 do
   def type(hand) do
     hand
     |> hand_value
-    |> Enum.sort(:desc)
+    |> Enum.sort(:asc)
     |> sorted_type()
   end
 
   # p2: 0 = joker
-  def sorted_type([b, b, b, b, 0]), do: 7
-  def sorted_type([a, b, b, b, 0]) when a != b and a != 0 and b != 0, do: 6
-  def sorted_type([a, a, a, b, 0]) when a != b and a != 0 and b != 0, do: 6
-  def sorted_type([a, a, b, b, 0]) when a != b and a != 0 and b != 0, do: 5
-
-  def sorted_type(hand = [_, _, _, _, 0]) do
+  def sorted_type(hand = [0 | _]) do
     num_jokers = Enum.count(hand, fn c -> c == 0 end)
     count_uniq = Enum.uniq(hand) |> Enum.count(fn c -> c != 0 end)
     case {num_jokers, count_uniq} do
-      {_, 0} -> 7 # j...
+      {_, 0} -> 7 # 0...
       {_, 1} -> 7 # a..., 0...
-      {3, 2} -> 6 # a, b, 0, 0, 0 = high card(1) + 3 --> four of a kind(6)
-      {2, 2} -> 6 # a, a, b, 0, 0 OR a, b, b, 0, 0 --> pair(2) + 2 = four of a kind(6)
-      {2, 3} -> 4 # a, b, c, 0, 0 --> high card(1) + 2 = three of a kind(4)
-      # should already be matched {1, 2} -> sorted_type(hand) # a, a, b, b, 0 OR # a, b, b, b, 0 OR # a, a, a, b, 0 --> 5 or 6
-      {1, 3} -> 4 # exactly one pair = three of a kind
-      {1, 4} -> 2 # high card + joker = one pair
+      {3, 2} -> 6 # a, b = high card(1) + 3 --> four of a kind(6)
+      {2, 2} -> 6 # a, a, b OR a, b, b --> pair(2) + 2 = four of a kind(6)
+      {2, 3} -> 4 # a, b, c --> high card(1) + 2 = three of a kind(4)
+      # a, a, a, b OR a, a, b, b
+      {1, 2} -> case frequencies(hand), do: ([1, 1, 3] -> 6; [1, 2, 2] -> 5)
+      {1, 3} -> 4 # a, a, b, c --> one pair + 1 = three of a kind(4)
+      {1, 4} -> 2 # a, b, c, d --> high card + joker = one pair
     end
   end
 
@@ -95,11 +91,11 @@ defmodule Year2023.Day07 do
 
     iex> Year2023.Day07.sorted_type([0, 0, 0, 0, 0])
     7
-    iex> Year2023.Day07.sorted_type([1, 1, 0, 0, 0])
+    iex> Year2023.Day07.sorted_type([0, 0, 0, 1, 1])
     7
     iex> Year2023.Day07.sorted_type([1, 1, 1, 1, 1])
     7
-    iex> Year2023.Day07.sorted_type([1, 1, 1, 1, 0])
+    iex> Year2023.Day07.sorted_type([0, 1, 1, 1, 1])
     7
     iex> Year2023.Day07.sorted_type([5, 1, 1, 1, 1])
     6
@@ -107,37 +103,41 @@ defmodule Year2023.Day07 do
     5
     iex> Year2023.Day07.sorted_type([5, 6, 1, 1, 1])
     4
-    iex> Year2023.Day07.sorted_type([6, 1, 1, 1, 0])
+    iex> Year2023.Day07.sorted_type([0, 6, 1, 1, 1])
     6
     iex> Year2023.Day07.sorted_type([5, 6, 6, 1, 1])
     3
-    iex> Year2023.Day07.sorted_type([6, 6, 1, 1, 0])
+    iex> Year2023.Day07.sorted_type([0, 6, 6, 1, 1])
     5
     iex> Year2023.Day07.sorted_type([5, 6, 7, 1, 1])
     2
-    iex> Year2023.Day07.sorted_type([6, 7, 1, 1, 0])
+    iex> Year2023.Day07.sorted_type([0, 6, 7, 1, 1])
     4
     iex> Year2023.Day07.sorted_type([5, 6, 7, 2, 1])
     1
-    iex> Year2023.Day07.sorted_type([5, 6, 7, 2, 0])
+    iex> Year2023.Day07.sorted_type([0, 5, 6, 7, 2])
     2
   """
-  def sorted_type([a, a, a, a, a]), do: 7 # five
-  def sorted_type([a, a, a, a, _]), do: 6 # four
-  def sorted_type([_, a, a, a, a]), do: 6
-  def sorted_type([b, b, a, a, a]), do: 5 # full house
-  def sorted_type([a, a, a, b, b]), do: 5
-  def sorted_type([a, a, a | _]), do: 4   # three
-  def sorted_type([_, a, a, a, _]), do: 4
-  def sorted_type([_, _, a, a, a]), do: 4
   def sorted_type(hand) do # two pair, one pair or high card
-    count_uniq = Enum.uniq(hand) |> Enum.count
-    case count_uniq do
-      3 -> 3
-      4 -> 2
-      5 -> 1
-    end
+    hand
+      |> frequencies
+      |> type_from_frequencies
   end
+
+  def frequencies(hand) do
+    hand
+      |> Enum.frequencies
+      |> Enum.map(&Elf.snd/1)
+      |> Enum.sort
+  end
+
+  def type_from_frequencies([1, 1, 1, 1, 1]), do: 1
+  def type_from_frequencies([1, 1, 1, 2]), do: 2
+  def type_from_frequencies([1, 2, 2]), do: 3
+  def type_from_frequencies([1, 1, 3]), do: 4
+  def type_from_frequencies([2, 3]), do: 5
+  def type_from_frequencies([1, 4]), do: 6
+  def type_from_frequencies([5]), do: 7
 
   @doc ~S"""
   ## Examples
