7fbf6dc542afc37b733416bba45c22143973c7ea
Stan Ionitoiu-Janssen <mail@janssen.io>
2023-12-07 22:22:59 +0100

    2023.8 Generated files with new generator

---
+++ Elixir/lib/2023/08.ex
@@ -0,0 +1,19 @@
+defmodule Year2023.Day08 do
+  @doc ~S"""
+  iex> AdventOfCode.example(2023, 08)
+  ...> |> Year2023.Day08.p1
+  0
+  """
+  def p1(lines) do
+    0
+  end
+
+  @doc ~S"""
+  iex> AdventOfCode.example(2023, 08)
+  ...> |> Year2023.Day08.p2
+  0
+  """
+  def p2(lines) do
+    0
+  end
+end
---
+++ Elixir/test/2023/08_test.exs
@@ -0,0 +1,4 @@
+defmodule Year2023.Day08Tests do
+  use ExUnit.Case
+  doctest Year2023.Day08, tags: [year: 2023, day: 08]
+end
