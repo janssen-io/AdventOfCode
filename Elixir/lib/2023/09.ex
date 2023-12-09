898885ffff8c47dc2f5ed78c08979bb597c0e5c6
Stan Ionitoiu-Janssen <mail@janssen.io>
2023-12-09 19:43:19 +0100

    2023.9 Clean up

--- Elixir/lib/2023/09.ex
+++ Elixir/lib/2023/09.ex
@@ -6,36 +6,32 @@ defmodule Year2023.Day09 do
   """
   def p1(lines) do
     lines
-    |> Stream.map(&(Elf.parse_ints(&1, :-)))
-    |> Stream.map(&extend_series/1)
-    |> Enum.reduce(0, fn {_, last}, acc -> acc + last end)
+    |> Stream.map(&Elf.parse_ints(&1, :-))
+    |> Stream.map(&extend_series(&1, fn xs, inc -> List.last(xs) + inc end))
+    |> Enum.sum()
   end
 
   @doc ~S"""
   iex> AdventOfCode.example(2023, 09)
   ...> |> Year2023.Day09.p2
-  0
+  2
   """
   def p2(lines) do
     lines
-    |> Stream.map(&(Elf.parse_ints(&1, :-)))
-    |> Stream.map(&extend_backwards/1)
-    |> Enum.reduce(0, fn [h|_], acc -> acc + h end)
+    |> Stream.map(&Elf.parse_ints(&1, :-))
+    |> Stream.map(&extend_series(&1, fn xs, inc -> hd(xs) - inc end))
+    |> Enum.sum()
   end
 
   @doc ~S"""
-  # iex> Year2023.Day09.difference_list([1,2,3])
-  # {[1,1], true, 1}
+  iex> Year2023.Day09.difference_list([1,2,3])
+  {[1,1], true}
   iex> Year2023.Day09.difference_list([1,1,2,3,5,8,13])
-  {[0,1,1,2,3,5], false, 5}
+  {[0,1,1,2,3,5], false}
   """
   def difference_list(xs) do
     {diff, is_stable} = difference_list({[], true}, xs)
-    {Enum.reverse(diff), is_stable, hd(diff)}
-  end
-
-  def difference_list({acc, is_stable}, [a, b | []]) do
-    {[b - a | acc], is_stable}
+    {Enum.reverse(diff), is_stable}
   end
 
   def difference_list({[c | acc], is_stable}, [a, b | xs]) do
@@ -48,40 +44,25 @@ defmodule Year2023.Day09 do
     |> difference_list([b | xs])
   end
 
+  def difference_list(result, [_]), do: result
+
   @doc ~S"""
-  # iex> Year2023.Day09.extend_series([1,2,3])
-  # {[1,2,3,4], 4}
+  iex> Year2023.Day09.extend_series([1,2,3])
+  4
   iex> Year2023.Day09.extend_series([1,3,6,10,15,21])
-  {[1,3,6,10,15,21,28], 28}
+  28
+  iex> Year2023.Day09.extend_series([1,1,2,3,5])
+  11
   """
-  def extend_series(xs) do
-    {diff, is_stable, last} = difference_list(xs)
+  def extend_series(xs, f) do
+    {diff, is_stable} = difference_list(xs)
 
-    if is_stable do
-      append_add(xs, last)
+    increment = if is_stable do
+      hd(diff)
     else
-      {_, new_last} = extend_series(diff)
-      append_add(xs, new_last)
+      extend_series(diff, f)
     end
+
+    f.(xs, increment)
   end
-
-  def append_add([x], last), do: {[x, x + last], x + last}
-
-  def append_add([x | xs], last) do
-    {ys, n} = append_add(xs, last)
-    {[x | ys], n}
-  end
-
-  def extend_backwards(xs) do
-    {diff, is_stable, _} = difference_list(xs)
-
-    if is_stable do
-      prepend_subtract(xs, diff)
-    else
-      new_diff = extend_backwards(diff)
-      prepend_subtract(xs, new_diff)
-    end
-  end
-
-  def prepend_subtract([x|xs], [y|_]), do: [x-y|xs]
 end
