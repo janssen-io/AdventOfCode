cc225479725b66e72e1084382f2853c6c9373806
Stan Ionitoiu-Janssen <mail@janssen.io>
2023-12-13 01:15:23 +0100

    2023.13 Init

---
+++ Elixir/lib/2023/13.ex
@@ -0,0 +1,19 @@
+defmodule Year2023.Day13 do
+  @doc ~S"""
+  iex> AdventOfCode.example(2023, 13)
+  ...> |> Year2023.Day13.p1
+  0
+  """
+  def p1(lines) do
+    0
+  end
+
+  @doc ~S"""
+  iex> AdventOfCode.example(2023, 13)
+  ...> |> Year2023.Day13.p2
+  0
+  """
+  def p2(lines) do
+    0
+  end
+end
---
+++ Elixir/test/2023/13_test.exs
@@ -0,0 +1,4 @@
+defmodule Year2023.Day13Tests do
+  use ExUnit.Case
+  doctest Year2023.Day13, tags: [year: 2023, day: 13]
+end
