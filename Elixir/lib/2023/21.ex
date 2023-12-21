0f9a24ba5c311d9ba60021d74de3fae793d1b2ac
Stan Ionitoiu-Janssen <mail@janssen.io>
2023-12-21 08:19:12 +0100

    2023.21 p1

---
+++ Elixir/lib/2023/21.ex
@@ -0,0 +1,76 @@
+defmodule Year2023.Day21 do
+  import Point, only: [+++: 2]
+
+  @doc ~S"""
+  iex> AdventOfCode.example(2023, 21)
+  ...> |> Year2023.Day21.p1(6)
+  16
+  """
+  def p1(lines, times \\ 64) do
+    parse_grid(lines)
+    |> simulate(times)
+    |> MapSet.size()
+  end
+
+  @doc ~S"""
+  iex> AdventOfCode.example(2023, 21)
+  ...> |> Year2023.Day21.p2
+  0
+  """
+  def p2(lines) do
+    0
+  end
+
+  def simulate(grid, n) do
+    {start, _} = Map.to_list(grid) |> Enum.find(fn {_, c} -> c == "S" end)
+    1..n
+    |> Enum.reduce(MapSet.new([start]), fn _, locs ->
+      l = tick(grid, locs)
+      # print(grid, l)
+      # IO.read(:line)
+      l
+    end)
+  end
+
+  def tick(grid, locations) do
+    Enum.reduce(locations, MapSet.new(), fn coord, new_locs ->
+      [{1, 0}, {-1, 0}, {0, 1}, {0, -1}]
+      |> Enum.reduce(new_locs, fn delta, locs ->
+        if Map.get(grid, coord +++ delta, "#") != "#" do
+          MapSet.put(locs, coord +++ delta)
+        else
+          locs
+        end
+      end)
+    end)
+  end
+
+  def parse_grid(lines) do
+    Enum.with_index(lines)
+    |> Enum.reduce(%{}, fn {line, y}, g ->
+      String.codepoints(line)
+      |> Enum.with_index()
+      |> Enum.reduce(g, fn {c, x}, gg ->
+        Map.put(gg, {x, y}, c)
+      end)
+    end)
+  end
+
+  def print(grid, locations) do
+    {max_x, _} = Map.keys(grid) |> Enum.max_by(&Elf.fst/1)
+    {_, max_y} = Map.keys(grid) |> Enum.max_by(&Elf.snd/1)
+    IO.inspect(locations)
+    for y <- 0..max_y do
+      for x <- 0..max_x do
+        c = Map.get(grid, {x, y})
+        cond do
+          c == '#' && c in locations -> raise "Garden plot on rocks"
+          {x, y} in locations -> IO.write("O")
+          true -> IO.write(c)
+        end
+      end
+      IO.write("\n")
+    end
+  end
+
+end
---
+++ Elixir/test/2023/21_test.exs
@@ -0,0 +1,4 @@
+defmodule Year2023.Day21Tests do
+  use ExUnit.Case
+  doctest Year2023.Day21, tags: [year: 2023, day: 21]
+end
